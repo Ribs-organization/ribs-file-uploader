cd5ba4424d74a8e0b22da2e2e7c0a9a62e9e4888
Piou-piou <pilloud.anthony@gmail.com>
2020-05-07 12:29:17 +0200

    [func] add drag enter and out events

--- source/js/RibsFileUploader.js
+++ source/js/RibsFileUploader.js
@@ -4,9 +4,14 @@ class RibsFileUploader {
 
     uploaderInputs.forEach((element) => {
       this.initHtmlElements(element);
+      this.initEventListeners();
     });
   }
 
+  /**
+   * method to create all html elements of the drag file uploader
+   * @param uploader
+   */
   initHtmlElements(uploader) {
     const parentUploaderDiv = document.createElement('div');
     parentUploaderDiv.classList.add('ribs-fileuploader');
@@ -32,6 +37,60 @@ class RibsFileUploader {
     progressBar.append(progressBarProgress);
 
     parentUploaderDiv.append(document.createElement('div'));
+  }
+
+  /**
+   * method to init all events listeners on uploader divs
+   */
+  initEventListeners() {
+    this.initPreventDefaultsEvents();
+    this.initDragEnterEvents();
+    this.initDragOutEvents();
+  }
+
+  /**
+   * add events prevent defaults to do not show file on drop in page
+   */
+  initPreventDefaultsEvents() {
+    ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+      document.body.addEventListener(eventName, (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+      }, false);
+
+      document.querySelectorAll('.ribs-fileuploader').forEach((element) => {
+        element.addEventListener(eventName, event => (event) => {
+          event.preventDefault();
+          event.stopPropagation();
+        }, false);
+      });
+    });
+  }
+
+  /**
+   * method to add class is-dragover when enter on uploader zone with files
+   */
+  initDragEnterEvents() {
+    ;['dragenter', 'dragover'].forEach(eventName => {
+      document.querySelectorAll('.ribs-fileuploader').forEach((element) => {
+        element.addEventListener(eventName, () => {
+          element.classList.add('is-dragover');
+        }, false);
+      });
+    })
+  }
+
+  /**
+   * method to add class is-dragover when qui drag of uploader or drop file
+   */
+  initDragOutEvents() {
+    ;['dragleave', 'drop'].forEach(eventName => {
+      document.querySelectorAll('.ribs-fileuploader').forEach((element) => {
+        element.addEventListener(eventName, () => {
+          element.classList.remove('is-dragover');
+        }, false);
+      });
+    })
   }
 }
 
